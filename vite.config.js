6b2e385248084cd1ec6bddcbc540f7a244f3fbe2
souf989 <soufiane.989@gmail.com>
2025-10-04 21:48:36 +0200

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
   },
-  plugins: [vue()],
+  plugins: [
+      vue(),
+    Sitemap({
+      hostname: 'https://enovat.fr',  // Remplace par ton domaine
+      // Optionnel : routes dynamiques si tu en as
+      dynamicRoutes: ["/methode", "/nous-contacter" , "/a-propos"
+        // Exemples : '/methode', '/articles/quelque-chose'
+      ],
+      outDir: 'dist', // par défaut
+      extensions: ['html'], // par défaut ; ce qui est généré
+      // autres options : changefreq, priority, etc. :contentReference[oaicite:1]{index=1}
+    })],
   resolve: {
     alias: [
       {
