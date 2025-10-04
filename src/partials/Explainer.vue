3180c5496e17223b5ad33b96b4da4c817dded52f
souf989 <soufiane.989@gmail.com>
2025-10-04 23:38:52 +0200

    PERF

--- src/partials/Explainer.vue
+++ src/partials/Explainer.vue
@@ -33,13 +33,15 @@
             </div>
             -->
           </div>
-
+          <!--
           <Modal id="video-modal" ariaLabel="modal-headline" :modalOpen="videoModalOpen" @close-modal="videoModalOpen = false">
             <video ref="video" class="w-full aspect-video" width="1920" height="1080" loop controls>
               <source src="../videos/video.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>  
           </Modal>
+          -->
+
 
         </div>
 
